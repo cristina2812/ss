6740c51b1fcdabb407712eac927c901c266b1f0b
adriantiberiu <log.adrian@gmail.com>
2020-02-28 09:27:40 +0200

    Added FooterPage, FooterSteps and FooterTest

--- src/test/java/suite/TestSuite.java
+++ src/test/java/suite/TestSuite.java
@@ -3,16 +3,23 @@ package suite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-import test.GoogleTest;
-import test.HeaderTest;
-import test.RegisterTest;
-import test.SearchByKeywordTest;
+import test.*;
 
 @RunWith(Suite.class)
 @SuiteClasses({
 	//GoogleTest.class,
 	//RegisterTest.class,
 		HeaderTest.class,
+		CartTest.class,
+		CheckoutTest.class,
+		FooterTest.class,
+		HomepageTest.class,
+		LoginTest.class,
+		OrderTest.class,
+		ProductTest.class,
+		RegisterTest.class,
+		SearchByKeywordTest.class,
+		WishlistTest.class,
 })
 public class TestSuite {
 	
